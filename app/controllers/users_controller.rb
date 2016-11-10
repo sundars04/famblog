40b2d20c7e1673b316a8e48b9d8aa99a8a293fe3
sundars0 <sundars04@rediffmail.com>
2016-11-10 06:59:53 +0000

    signup and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def new
     @user = User.new  
   end
@@ -13,7 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit    
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Account details updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end

20c47135c17a9d2dea7ce64993a4832da7a05848
Sundars0 <Sundars04@outlook.com>
2016-12-29 08:56:23 +0000

    add scope to category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,6 @@ class Category < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3, maximum: 30}
   validates_uniqueness_of :name
 
+  scope :sorted, -> {order("created_at DESC")}
+
 end
